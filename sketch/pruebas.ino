6a010087ba8282949ebbf91ede08b10dd1740155
patodabju <patodabju@gmail.com>
2026-03-19 14:15:26 -0600

    Update pruebas.ino

--- sketch/pruebas.ino
+++ sketch/pruebas.ino
@@ -1,10 +1,33 @@
+int pins[] = {3, 4, 5, 6, 9, 10, 11};  // tus servos
+int numServos = 7;
+
+int pulse = 1500;
+int target = 1500;
+
 void setup() {
-  pinMode(9, OUTPUT);
+  for (int i = 0; i < numServos; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
+
+  randomSeed(analogRead(0));  // para random real
 }
 
 void loop() {
-  digitalWrite(9, HIGH);
-  delayMicroseconds(1500);   // centro aproximado
-  digitalWrite(9, LOW);
-  delayMicroseconds(18500);  // periodo total ~20 ms
+
+  for (int i = 0; i < numServos; i++) {
+
+    // elegir posición aleatoria (1000 a 2000 us)
+    target = random(1000, 2000);
+
+    // mantener señal por un rato (para que el servo llegue)
+    for (int t = 0; t < 80; t++) {
+
+      digitalWrite(pins[i], HIGH);
+      delayMicroseconds(target);
+      digitalWrite(pins[i], LOW);
+      delayMicroseconds(20000 - target);
+    }
+
+    delay(500); // pausa entre servos
+  }
 }
