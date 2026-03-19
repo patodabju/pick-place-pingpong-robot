50496efa4a29d6f4001e8589e209409c9972ff9e
patodabju <patodabju@gmail.com>
2026-03-19 14:09:07 -0600

    Update pruebas.ino

--- sketch/pruebas.ino
+++ sketch/pruebas.ino
@@ -1,22 +1,10 @@
-int pins[] = {2,3,4,5,6,7,8,9,10,11,12,13};
-
 void setup() {
-  for (int i = 0; i < 12; i++) {
-    pinMode(pins[i], OUTPUT);
-  }
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 12; i++) {
-
-    // generar señal servo en ese pin
-    for (int j = 0; j < 100; j++) {
-      digitalWrite(pins[i], HIGH);
-      delayMicroseconds(1500);
-      digitalWrite(pins[i], LOW);
-      delay(20);
-    }
-
-    delay(1000); // pausa entre pines
-  }
+  digitalWrite(9, HIGH);
+  delayMicroseconds(1500);   // centro aproximado
+  digitalWrite(9, LOW);
+  delayMicroseconds(18500);  // periodo total ~20 ms
 }
