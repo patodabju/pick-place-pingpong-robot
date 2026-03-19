c17c4aa3ba80ec005c59142c6aa91017a82e8179
patodabju <patodabju@gmail.com>
2026-03-18 20:53:01 -0600

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,14 +1,9 @@
-from arduino import App
+from arduino.app_utils import App
 from arduino.app_bricks.web_ui import WebUI
 
 ui = WebUI()
 
+print("Iniciando prueba de livestream web...")
+print("Abre en tu navegador: http://<board-name>.local:7000")
 
-def main():
-    print("Iniciando prueba de livestream web...")
-    print("Abre en tu navegador: http://<board-name>.local:7000")
-    App.run()
-
-
-if __name__ == "__main__":
-    main()
+App.run()
