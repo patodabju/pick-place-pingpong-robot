990c39859b850cf561053aaea76a39090dc86584
patodabju <patodabju@gmail.com>
2026-03-18 19:41:39 -0600

    Update vision.color.detect.py

--- python/vision.color.detect.py
+++ python/vision.color.detect.py
@@ -1,27 +1,29 @@
 import cv2
+import platform
 
 
-def open_camera(camera_index=0, width=None, height=None, use_dshow=True):
-    if use_dshow:
+def open_camera(camera_index=0, width=640, height=360):
+    system_name = platform.system()
+
+    # Selección de backend según sistema operativo
+    if system_name == "Windows":
         cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
     else:
-        cap = cv2.VideoCapture(camera_index)
+        cap = cv2.VideoCapture(camera_index, cv2.CAP_V4L2)
 
     if not cap.isOpened():
         return None
 
-    if width is not None:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-
-    if height is not None:
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     return cap
 
 
 def main():
-    camera_index = 1   # prueba 0, 1, 2...
-    cap = open_camera(camera_index=camera_index, width=640, height=480, use_dshow=True)
+    camera_index = 2   # En tu UNO Q ya comprobaste que este índice funciona
+
+    cap = open_camera(camera_index=camera_index, width=640, height=360)
 
     if cap is None:
         print(f"Error: no se pudo abrir la cámara con índice {camera_index}.")
