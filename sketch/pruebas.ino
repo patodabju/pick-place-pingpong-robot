61a0d8e437c5c8c19e184a5681c03f35c5cb7fb0
patodabju <patodabju@gmail.com>
2026-03-19 14:20:53 -0600

    Update pruebas.ino

--- sketch/pruebas.ino
+++ sketch/pruebas.ino
@@ -1,33 +1,94 @@
-int pins[] = {3, 4, 5, 6, 9, 10, 11};  // tus servos
-int numServos = 7;
+#include <Servo.h>
 
-int pulse = 1500;
-int target = 1500;
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+
+const int servoPins[7] = {3, 4, 5, 6, 9, 10, 11};
+
+void centerAll() {
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+  servo5.write(90);
+  servo6.write(90);
+  servo7.write(90);
+}
 
 void setup() {
-  for (int i = 0; i < numServos; i++) {
-    pinMode(pins[i], OUTPUT);
-  }
+  servo1.attach(servoPins[0]);
+  servo2.attach(servoPins[1]);
+  servo3.attach(servoPins[2]);
+  servo4.attach(servoPins[3]);
+  servo5.attach(servoPins[4]);
+  servo6.attach(servoPins[5]);
+  servo7.attach(servoPins[6]);
 
-  randomSeed(analogRead(0));  // para random real
+  centerAll();
+  delay(3000);
 }
 
 void loop() {
+  // Servo 1
+  servo1.write(75);
+  delay(1200);
+  servo1.write(105);
+  delay(1200);
+  servo1.write(90);
+  delay(800);
 
-  for (int i = 0; i < numServos; i++) {
+  // Servo 2
+  servo2.write(75);
+  delay(1200);
+  servo2.write(105);
+  delay(1200);
+  servo2.write(90);
+  delay(800);
 
-    // elegir posición aleatoria (1000 a 2000 us)
-    target = random(1000, 2000);
+  // Servo 3
+  servo3.write(75);
+  delay(1200);
+  servo3.write(105);
+  delay(1200);
+  servo3.write(90);
+  delay(800);
 
-    // mantener señal por un rato (para que el servo llegue)
-    for (int t = 0; t < 80; t++) {
+  // Servo 4
+  servo4.write(75);
+  delay(1200);
+  servo4.write(105);
+  delay(1200);
+  servo4.write(90);
+  delay(800);
 
-      digitalWrite(pins[i], HIGH);
-      delayMicroseconds(target);
-      digitalWrite(pins[i], LOW);
-      delayMicroseconds(20000 - target);
-    }
+  // Servo 5
+  servo5.write(75);
+  delay(1200);
+  servo5.write(105);
+  delay(1200);
+  servo5.write(90);
+  delay(800);
 
-    delay(500); // pausa entre servos
-  }
+  // Servo 6
+  servo6.write(75);
+  delay(1200);
+  servo6.write(105);
+  delay(1200);
+  servo6.write(90);
+  delay(800);
+
+  // Servo 7
+  servo7.write(75);
+  delay(1200);
+  servo7.write(105);
+  delay(1200);
+  servo7.write(90);
+  delay(800);
+
+  delay(2000);
 }
